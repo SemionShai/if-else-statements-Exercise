94708ef81a1b8ab4aba5b1c58e3d1ceef5efc668
Semion Shai <104586835+SemionShai@users.noreply.github.com>
2022-05-17 21:35:07 +0300

    Add files via upload
    Watch only the js file

---
+++ if-else statement tasks/java.js
@@ -0,0 +1,37 @@
+const dolphin_first_score = 97;
+const dolphin_second_score = 112;
+const dolphin_third_score =101;
+
+const dolphin_total_score = dolphin_first_score + dolphin_second_score + dolphin_third_score;
+const dolphin_avg_score = dolphin_total_score / 3 ;
+
+const koala_first_score = 109;
+const koala_second_score =95;
+const koala_third_score =1;
+
+const koala_total_score = koala_first_score + koala_second_score + koala_third_score;
+const koala_avg_score = koala_total_score / 3;
+
+// if (koala_avg_score > dolphin_avg_score){
+//     console.log("The winner is KOALS!!!")
+// }
+// else if (koala_avg_score < dolphin_avg_score){
+//     console.log("The winner is Dolphins!!!")
+// }
+// else{
+//     console.log("Its a draw!!!")
+// }
+
+// if (koala_avg_score > 100){
+//     if (koala_avg_score > dolphin_avg_score){
+//         console.log("The winner is KOALS!!!")
+//     }
+// } else if (dolphin_avg_score > 100){
+//     if(koala_avg_score < dolphin_avg_score){
+//         console.log("The winner is Dolphins !!!")
+//     }
+// }else if(koala_avg_score === 100 || dolphin_avg_score === 100 ){
+//     console.log ("Its a draww!!!")
+// }else("The Teams disnt reach the minimum amount of points")
+
+
